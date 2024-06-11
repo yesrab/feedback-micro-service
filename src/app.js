87e9c85a99e60ae4226d7b72f9a0e41a885ad77e
yesrab <yesrab.ali111@gmail.com>
2024-06-11 18:29:40 +0530

    init feedback microservice

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+require("express-async-errors");
+require("dotenv").config();
+
+const middlewares = require("./middlewares");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
+

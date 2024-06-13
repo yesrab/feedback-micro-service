e3e13b4cebe35bd93cb8597196fefab1e282fc27
yesrab <yesrab.ali111@gmail.com>
2024-06-13 21:41:10 +0530

    added wild card origin

--- src/app.js
+++ src/app.js
@@ -11,7 +11,11 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
